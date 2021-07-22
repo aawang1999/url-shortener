a032b523528a5a8e65c9fc6a33b05b57c3f7cdf7
Tristan Wang <aawang99@gmail.com>
2021-07-22 09:28:32 -0700

    feat: create and render main.hbs

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-  res.send('Hello world.')
+  res.render('index')
 })
 
 app.listen(PORT, () => {
