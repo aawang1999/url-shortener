0608b1390357dafd6a3bd064771500b59944bbb8
Tristan Wang <aawang99@gmail.com>
2021-07-22 10:01:49 -0700

    modify: create routes

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
+const routes = require('./routes')
 const app = express()
 const PORT = 3000
 
@@ -9,14 +10,7 @@ app.set('view engine', 'hbs')
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.static('public'))
-
-app.get('/', (req, res) => {
-  res.render('index')
-})
-
-app.post('/', (req, res) => {
-  res.render('success', { url: req.body.url, shortUrl: '<Short URL here.>' })
-})
+app.use(routes)
 
 app.listen(PORT, () => {
   console.log(`Express is running on port ${PORT}.`)
