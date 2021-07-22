39a28b4261564050b5927f190681c9bd955bca90
Tristan Wang <aawang99@gmail.com>
2021-07-22 10:17:15 -0700

    chore: add and config mongoose

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const routes = require('./routes')
+require('./config/mongoose')
+
 const app = express()
 const PORT = 3000
 
