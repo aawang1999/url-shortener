efd30d61fbb1239043f9e1f43ae90c553b749dbe
Tristan Wang <aawang99@gmail.com>
2021-07-22 10:25:40 -0700

    feat: add checks for short url uniqueness

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,51 @@
 const express = require('express')
 const router = express.Router()
 const getShortUrl = require('../tools/general')
+const ShortUrl = require('../models/shortUrl')
 
 router.get('/', (req, res) => {
   res.render('index')
 })
 
-router.post('/', (req, res) => {
-  res.render('success', { url: req.body.url, shortUrl: getShortUrl(5) })
+router.post('/', async (req, res) => {
+  const url = req.body.url
+
+  let checkUrl = async (url) => {
+    try {
+      const shortUrl = await ShortUrl.find({ url }).lean().exec()
+      if (shortUrl.length === 1) {
+        return shortUrl[0].id
+      } else {
+        return false
+      }
+    } catch (error) {
+      return 'Error.'
+    }
+  }
+
+  let checkId = async (id) => {
+    try {
+      const findId = await ShortUrl.find({ id }).lean().exec()
+      return findId.length
+    } catch (error) {
+      return 'Error.'
+    }
+  }
+
+  let id = await checkUrl(url)
+
+  if (id) {
+    res.render('success', { url, id })
+  } else {
+    do {
+      id = getShortUrl(5)
+    } while (await checkId(id))
+    ShortUrl.create({ url, id })
+      .then(() => {
+        return res.render('success', { url, id })
+      })
+      .catch(error => console.log(error))
+  }
 })
 
 module.exports = router
