b3170b4cd63a9b3b91278969bb6e2c9872a6ad6a
Tristan Wang <aawang99@gmail.com>
2021-07-22 09:18:12 -0700

    chore: init project

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const app = express()
+const PORT = 3000
+
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+
+app.set('view engine', 'hbs')
+
+app.use(express.urlencoded({ extended: true }))
+app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.send('Hello world.')
+})
+
+app.listen(PORT, () => {
+  console.log(`Express is running on port ${PORT}.`)
+})
